033881eb44a7fb889db3383f61b8e2bbc3a5a274
Alexander Linovskiy <sasha@linovsky.com>
2017-05-16 02:57:31 +1000

    - Move payment related functions to react container level

--- src/PayPal.js
+++ src/PayPal.js
@@ -11,36 +11,35 @@ const ReactButton = window.paypal.Button.driver('react', {
 });
 
 class PaymentBtn extends Component {
+    payment() {
+       const env = this.props.env;
+       const client = this.props.client;
+       return window.paypal.rest.payment.create(env, client, {
+           transactions: [
+               {
+                   amount: {total: '1.00', currency: 'AUD'}
+               }
+           ]
+       });
+    }
+
+    onAuthorize(data, actions) {
+        // Optional: display a confirmation page here
+        return actions.payment.execute().then(function () {
+            // Show a success page to the buyer
+            alert('Your PayPal payment received, thank you!');
+            // TODO: clear selected items list by reloading data
+        });
+    }
+
     render() {
-        const opts = Object.assign({}, this.props,
-            {
-               payment() {
-                   let env = this.props.env;
-                   let client = this.props.client;
-                   return window.paypal.rest.payment.create(env, client, {
-                       transactions: [
-                           {
-                               amount: {total: '1.00', currency: 'AUD'}
-                           }
-                       ]
-                   });
-               },
-                onAuthorize(data, actions) {
-                    // Optional: display a confirmation page here
-                    return actions.payment.execute().then(function () {
-                        // Show a success page to the buyer
-                        alert('Your PayPal payment received, thank you!');
-                        // TODO: clear selected items list by reloading data
-                    });
-                }
-            }
-        );
+        const opts = Object.assign({}, this.props);
 
         return (
             <div>
                 <ReactButton env={opts.env} style={opts.style}
-                                     client={opts.client} payment={opts.payment}
-                                     commit={opts.commit} onAuthorize={opts.onAuthorize} />
+                                     client={opts.client} payment={() => this.payment()}
+                                     commit={opts.commit} onAuthorize={(data,actions) => this.onAuthorize(data,actions)} />
             </div>
         );
     }
