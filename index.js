b433781a3b30593af7998f5ef90757d7ce17665b
Alexander Linovskiy <sasha@linovsky.com>
2017-05-17 03:07:40 +1000

    - switch to Node for Heroku

---
+++ index.js
@@ -0,0 +1,17 @@
+const  express = require('express');
+const path = require('path');
+const  app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, './build', 'index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
