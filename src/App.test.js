daf78fb83e565afdfbf972b396d82ba5dd59a0d9
Alexander Linovskiy <sasha@linovsky.com>
2017-05-21 01:36:06 +1000

    add geolocation mock to the test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+global.navigator.geolocation = {
+  getCurrentPosition: jest.fn(),
+  watchPosition: jest.fn()
+};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
